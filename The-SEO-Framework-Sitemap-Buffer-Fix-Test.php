751a830a7c70e8d42155600b3f10471eaf762ee0
Sybre Waaijer <sybrew@gmail.com>
2016-03-28 14:03:57 +0200

    Initial Commit

---
+++ The-SEO-Framework-Sitemap-Buffer-Fix-Test.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Plugin Name: The SEO Framework - Sitemap Buffer Fix Test
+ * Plugin URI: https://wordpress.org/plugins/autodescription/
+ * Description: This is a test plugin to see whether buffer flushing before the SiteMap output works as expected.
+ * Version: 1.0.0
+ * Author: Sybre Waaijer
+ * Author URI: https://cyberwire.nl/
+ * License: GPLv3
+ */
+
+//tsf_test_broken_sitemap();
+/**
+ * Creates an empty space as soon as this plugin's loaded.
+ * For Testing purposes only.
+ *
+ * @since 1.0.0
+ */
+function tsf_test_broken_sitemap() {
+	?>
+
+	<?php // Broken sitemap emerges.
+}
+
+add_action( 'template_redirect', 'tsf_sitemap_buffer', 1 );
+/**
+ * Flushes the PHP buffer right before when The SEO Framework's sitemap is output.
+ * Also removes rogue header information, if any.
+ *
+ * @since 1.0.0
+ * @global $wp_query
+ */
+function tsf_sitemap_buffer() {
+
+	if ( '' !== get_option( 'permalink_structure' ) && function_exists( 'tsf_get_option' ) && tsf_get_option( 'sitemaps_output' ) ) {
+		global $wp_query;
+
+		if ( isset( $wp_query->query_vars['the_seo_framework_sitemap'] ) && 'xml' === $wp_query->query_vars['the_seo_framework_sitemap'] ) {
+			ob_clean();
+			header_remove();
+		}
+
+	}
+}
